5401641a324205cbf3bb3378550cd0e77964ecc3
Thomas Sillmann <contact@thomassillmann.de>
2021-11-15 15:59:19 +0100

    Created content for video 02-Solution

--- Challenges/Operator Challenge.playground/Contents.swift
+++ Challenges/Operator Challenge.playground/Contents.swift
@@ -1,5 +1,13 @@
 // Operator Challenge
 
+postfix operator ++
+
+extension Int {
+    static postfix func ++ (value: inout Int) {
+        value += 1
+    }
+}
+
 var someValue = 19
 someValue++
 print("Some value: \(someValue)")
