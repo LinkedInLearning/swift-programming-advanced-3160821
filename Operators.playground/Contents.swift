28fe74d5a05d7e655d8e45b4e47725ef66d94fb6
Thomas Sillmann <contact@thomassillmann.de>
2021-11-15 13:04:40 +0100

    Created content for video 02-02

--- Operators.playground/Contents.swift
+++ Operators.playground/Contents.swift
@@ -6,3 +6,14 @@ let value = 19
 var valueInfo: String
 
 valueInfo = value > 10 ? "Größer 10" : "Kleiner gleich 10"
+
+
+
+
+// Nil-Coalescing Operator
+
+let defaultName = "Max Mustermann"
+var name: String?
+var nameToUse: String
+
+nameToUse = name ?? defaultName
